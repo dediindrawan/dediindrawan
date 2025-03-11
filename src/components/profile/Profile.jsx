40dde85e0fea9efe28dcce8a6183b1552aad4fb8
dediindrawan <dediindrawan812@gmail.com>
2025-03-11 18:37:35 +0700

    update: update description in profile section

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -6,8 +6,8 @@ const Profile = () => {
       <img src={ProfileImage} alt="image" loading="lazy" className="reveal-left" />
       <figcaption>
         <article className="reveal-bottom">
-          As a passionate Front End Developer with over one year of experience, I specialize in crafting responsive and user friendly websites using HTML, CSS, and JavaScript. I am currently enhancing my skills in React.js to build dynamic
-          and engaging web applications. Additionally, I create web programming content, sharing insights and tutorials to help others learn and grow in the field.
+          As a passionate Front End Developer with over one year of experience, I specialize in crafting responsive and user friendly websites using React.js, TailwindCSS, and JavaScript. I am currently enhancing my skills in React.js to
+          build more dynamic and efficient web applications. Additionally, I create web programming content, sharing insights and tutorials to help others learn and grow in the field.
         </article>
       </figcaption>
     </figure>
