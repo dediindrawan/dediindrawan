2085161a6773073086626d49c85e79f8c18c81cf
dediindrawan <dediindrawan812@gmail.com>
2025-03-11 18:26:34 +0700

    update: update description in jumbotron

--- src/components/jumbotron/index.jsx
+++ src/components/jumbotron/index.jsx
@@ -6,7 +6,8 @@ const Jumbotron = () => {
       <h1 className="fade-in-top">Dedi Indrawan</h1>
       <h2 className="fade-in-top">Front End Developer & Content Creator</h2>
       <p className="fade-in-top">
-        <strong>Front End Developer</strong> with 1+ years of experience creating responsive and user friendly websites using HTML, CSS, JavaScript, and currently expanding my skills in React.js.
+        <strong>Front End Developer</strong> with 1+ years of experience creating responsive and user friendly websites using React.js, TailwindCSS, JavaScript, and continuously enhancing skills in React.js to build more dynamic and
+        efficient web applications.
       </p>
 
       <ul className="fade-in-bottom">
